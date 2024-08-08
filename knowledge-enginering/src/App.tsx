f688760fa64e0460d56aa579d5d916dc360ba3b0
fadichahineOS <fsamchawork@protonmail.com>
2024-08-08 15:56:06 +0100

    added routes for contact pages

--- knowledge-enginering/src/App.tsx
+++ knowledge-enginering/src/App.tsx
@@ -6,6 +6,8 @@ import Article from './components/article';
 import SignIn from './components/signIn';
 import WriterProfile from './components/writerProfile';
 import ReaderProfile from './components/readerProfile';
+import ContactUs from './components/contactUs';
+import ContactConfirmation from './components/contactConfirmation';
 
 const App: React.FC = () => {
   return (
@@ -13,6 +15,8 @@ const App: React.FC = () => {
       <Route path="/" element={<LandingPage />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/contact" element={<ContactUs />} />
+      <Route path="/contact-confirmation" element={<ContactConfirmation/>} />
       <Route path="/article" element={<Article/>} />
       <Route path="/writer-profile" element={<WriterProfile />} />
       <Route path="/reader-profile" element={<ReaderProfile />} />
